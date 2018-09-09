a0b3e28eb3776233cfd2f41c8d79707c81321f73
kabitacode <siapaa249@gmail.com>
2018-09-09 14:54:59 +0700

    update

--- js/custom.js
+++ js/custom.js
@@ -74,46 +74,6 @@ var windowHeight =$(window).height();
 $('.banner').css({'height':windowHeight });
 
 
-// for portfoli filter jquary
-$(window).load(function(){
-    var $container = $('.portfolioContainer');
-    $container.isotope({
-        filter: '*',
-        animationOptions: {
-            duration: 750,
-            easing: 'linear',
-            queue: false
-        }
-    });
- 
-    $('.portfolioFilter a').click(function(){
-        $('.portfolioFilter .current').removeClass('current');
-        $(this).addClass('current');
- 
-        var selector = $(this).attr('data-filter');
-        $container.isotope({
-            filter: selector,
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-         });
-         return false;
-    }); 
-
-
-
-// for portfoli lightbox jquary
-jQuery(function() {
-	var $chosenSheet,
-	$stylesheets = $( "a[id^=theme-]" );
-	
-	// run rlightbox
-	$( ".lb" ).rlightbox();
-	$( ".lb_title-overwritten" ).rlightbox({overwriteTitle: true});
-});
-});
 
 // Somth page scroll
 $(function() {
